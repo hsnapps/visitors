3ffa1ff24b054f641d9d48dff674db38096092f8
Hassan Ali <prog.hasan@gmail.com>
2019-04-22 18:54:55 +0300

    Change email message title

--- resources/views/emails/order_placed.blade.php
+++ resources/views/emails/order_placed.blade.php
@@ -1,5 +1,5 @@
 @component('mail::message')
-# Introduction
+# ORDER CONFIRMATION
 
 <img src="http://eos-ksa.com/images/rsos-2019-site-logo.png" alt="RSOS 2019">
 <h3>you have been successfully subscribed to the following events using your credit card (VISA) ending with (***0).</h3>
